73228670b1c30d60569381d66aea5650dfb769e9
brarnedo <brarnedo@gmail.com>
2024-04-24 09:12:02 -0300

    Inicio de css master BA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import PruebaBA from '../src/pages/pruebaBA.jsx'
+
 
 function App() {
 	return (
-		<h1 className="text-red-600">HOLA TEST</h1>
+		<PruebaBA />
 	);
 }
 
---
+++ src/pages/pruebaBA.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function PruebaBA() {
+  return (
+    <>
+        <nav>
+            <div>
+                <span className='titulo grey-black'> e-zense</span>
+            </div>
+
+            <div className='nav-opciones'>
+                <span className='opciones'> Works</span>
+                <span className='opciones'> Bureau</span>
+                <span className='opciones'> Services</span>
+                <span className='opciones'> Clients</span>
+                <span className='opciones'> Team</span>                    
+            </div>
+
+            <div>
+                
+            </div>
+          
+        </nav>
+    </>
+      
+    
+  )
+}
+
+
