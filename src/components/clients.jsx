e5641b1a7e011a596f0b185f1528bda007b85ee0
brarnedo <brarnedo@gmail.com>
2024-04-26 09:37:46 -0300

    primer Prototipo de Titulo

--- src/components/clients.jsx
+++ src/components/clients.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import TituloSeccion from "./tituloSeccion";
-import '../css/works.css';
 
-const Works = () => {
+import TituloSeccionR from "./tituloSeccionR";
+import '../css/clients.css';
+
+const Clients = () => {
   return (
     <>
-    <div className="works_content">
-      <TituloSeccion titulo="Works"/>
+    <div className="clients_content">
+    <TituloSeccionR titulo="Clients"/>
       <div className="seccion-subTitulo">
         <span className="text-md-lato-700 grey-black"> Unites aesthetic innovation with technical prowess. </span>
         
         <span className="text-md-lato-400 grey-black"> From strategic planning that charts your digital destiny to Intuitive UX Design paired with advanced Coding Techniques, we architect and implement ideas into impactful digital realities </span>
       </div>
     </div>
+
     </>
   );
 };
 
-export default Works;
+export default Clients;
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -3,12 +3,14 @@ import Banner from "../components/banner";
 import Works from "../components/works";
 import Services from "../components/services";
 import Bureau from "../components/bureau";
+import Clients from "../components/clients";
 
 const home = () => {
   return (
     <>
       <Banner />
       <Works />
+      <Clients />
       <Services />
       <Bureau />
     </>
