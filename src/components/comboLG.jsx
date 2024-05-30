087c707d1a224ec15311d23331dd6887b865acc3
jeremias256 <jere.menacho@gmail.com>
2024-05-30 15:41:18 -0300

    fixes en comboLG

--- src/components/comboLG.jsx
+++ src/components/comboLG.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
-
 import "../css/comboLG.css";
+
 import biselBig from "../assets/LogosCoca.png";
 import biselBigM from "../assets/LogosCocaM.png";
 import biselBigL from "../assets/LogosCodaL.png";
 
 const ComboLG = () => {
+  // Para ver la resolución de la pantalla
+
   useEffect(() => {
     function handleResize() {
       const screenWidth = window.innerWidth;
@@ -24,7 +26,7 @@ const ComboLG = () => {
 
   return (
     <>
-      <div className="combo_content3 mt-[24px] flex">
+      <div className="combo_content3">
         <div className="ca">
           <div className="ca-f1">
             <div className="ca-f1-f1">
